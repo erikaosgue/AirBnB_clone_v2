857e03de4c3ffc00a004e9e1b8752fbd866e287c
erikaosgue <1476@holbertonschool.com>
2020-08-18 03:44:18 +0000

    Add task 1: Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,13 +10,11 @@ from os import path
 
 def do_pack():
     """create the folder versions if not exists """
-    try:
-        if path.isdir("versions") is False:
-            local("mkdir -p versions")
-        t = datetime.now()
-        name = t.strftime("%Y%m%d%H%M%S")
-        archive_name = "web_static_{}.tgz".format(name)
-        local("tar -czvf versions/{} web_static".format(archive_name))
-        return "versions/" + archive_name
-    except:
-        return None
+    if path.isdir("versions") is False:
+        local("mkdir -p versions")
+
+    t = datetime.now()
+    name = t.strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(name)
+    local("tar -czvf versions/{} web_static".format(archive_name))
+    return "versions/" + archive_name
