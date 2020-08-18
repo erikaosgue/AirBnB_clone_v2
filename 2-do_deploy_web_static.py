64d4ea6386c90a9dc8d7706940919b0455d11bf2
erikaosgue <1476@holbertonschool.com>
2020-08-18 03:32:31 +0000

    Update task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,29 +7,32 @@ import os.path
 from os import path
 from fabric.api import env, put, run
 
-env.host = ['35.237.57.250', '34.75.204.221']
+env.hosts = ["35.237.57.250", "34.75.204.221"]
 
 
 def do_deploy(archive_path):
     """ Deploy archive!"""
 
+    if path.isfile(archive_path) is False:
+        return False
     # archive_path is the hole path
     # ej --> versions/web_static_20170315003959.tgz -i
     # my_ssh_private_key -u ubuntu
 
     # archive_name is the name of the archive file with extension.tgz
     # ej --> web_static_20170315003959.tgz
-    archive_name = archive_path.split("/")[1]
+    archive_name = archive_path.split("/")[-1]
 
     # name archive file without the extension .tgz
     # ej --> web_static_20170315003959
     name = archive_name.split(".")[0]
 
-    if path.isfile(archive_path) is False:
-        return False
     if put(archive_path, "/tmp/{}".format(archive_name)).failed is True:
         return False
-    if put("mkdir -p /data/web_static/releases/{}/".
+    # if run("rm -rf /data/web_static/releases/{}/".
+    #        format(name)).failed is True:
+    #     return False
+    if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
